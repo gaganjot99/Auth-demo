b0bfd82a153e9551356dc125b6e01df79d8da157
Gagan Kahal <73100470+gaganjot99@users.noreply.github.com>
2022-12-27 12:44:26 +0530

    fix: ui changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Login from "./Login";
-import { Route, Routes, useNavigate } from "react-router-dom";
-import React, { Suspense } from "react";
+import { Route, Routes, useNavigate, useLocation } from "react-router-dom";
+import React, { Suspense, useEffect, useState } from "react";
 import Loader from "./components/Others/Loader";
 import ErrorBoundary from "./components/Others/ErrorBoundary";
 
@@ -9,7 +9,29 @@ const Signup = React.lazy(() => import("./Signup"));
 const Main = React.lazy(() => import("./Main"));
 
 function App() {
+  const [user, setUser] = useState(false);
   const navigate = useNavigate();
+  const location = useLocation();
+  useEffect(() => {
+    if (location.pathname !== "/") {
+      return;
+    }
+    fetch("/api/data/user")
+      .then((data) => {
+        if (data.redirected) {
+          navigate("/login");
+        } else {
+          return data.json();
+        }
+      })
+      .then((data) => {
+        if (!data) {
+          navigate("/login");
+        } else {
+          setUser(true);
+        }
+      });
+  }, []);
 
   return (
     <div className="App">
@@ -17,7 +39,12 @@ function App() {
         <Suspense fallback={<Loader></Loader>}>
           <ErrorBoundary>
             <Routes>
-              <Route path="/" element={<Main navigate={navigate} />} />
+              <Route
+                path="/"
+                element={
+                  user ? <Main navigate={navigate} /> : <Loader></Loader>
+                }
+              />
               <Route path="/login" element={<Login navigate={navigate} />} />
               <Route path="/signup" element={<Signup navigate={navigate} />} />
             </Routes>
--- src/Main.js
+++ src/Main.js
@@ -1,28 +1,11 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Navicon from "./components/Nav";
 import ErrorBoundary from "./components/Others/ErrorBoundary";
 import Righthalf from "./components/Righthalf";
 
 const Main = ({ navigate }) => {
-  const [user, setUser] = useState("");
   const [showList, setShowList] = useState(true);
-  useEffect(() => {
-    fetch("/api/data/user")
-      .then((data) => {
-        if (data.redirected) {
-          navigate("/login");
-        } else {
-          return data.json();
-        }
-      })
-      .then((data) => {
-        if (!data) {
-          navigate("/login");
-        } else {
-          setUser(data);
-        }
-      });
-  }, [navigate]);
+
   return (
     <div id="main-app">
       <Navicon setShowList={setShowList} showList={showList} />
